b6b0f394176fc1ed069c9a8f0ee3a01e5944ea52
Kian Zareinajad <kzareinajad@gmail.com>
2023-09-22 02:51:00 +0100

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,29 +7,29 @@ function Contact() {
     <div className="flex w-full max-w-6xl justify-center items-center ">
         <section className='flex flex-col justify-center items-center mt-20 w-full max-w-3xl p-4'>
             <h1 className='flex justify-center text-6xl font-bold p-8"'>Contact Me</h1>
-            <span className='p-2 text-xl '>Please fill out the form below to discuss any work opportunites</span>
+            <span className='flex justify-center items-center p-2 text-xl '>Please fill out the form below to discuss any work opportunites</span>
             <form className='flex flex-col w-[90vw] max-w-2xl text-lg'>
-                <input type="text" placeholder='Your Name' className='border h-10 px-4 py-7 bg-slate-700 m-2 rounded-lg'/>
-                <input type="text" placeholder='Your Email' className='border h-10 px-4 py-7 bg-slate-700 m-2 rounded-lg'/>
-                <textarea name="message"  rows="5" placeholder='Your Message' className='p-4 bg-slate-700 m-2 rounded-lg'></textarea>
-                <div className='flex justify-center m-4'>
+                <input type="text" placeholder='Your Name' className='border h-10 px-4 py-7 bg-slate-700 m-1 rounded-lg'/>
+                <input type="text" placeholder='Your Email' className='border h-10 px-4 py-7 bg-slate-700 m-1 rounded-lg'/>
+                <textarea name="message"  rows="5" placeholder='Your Message' className='p-4 bg-slate-700 m-1 rounded-lg'></textarea>
+                <div className='flex justify-center m-2'>
                 <button type='submit' value='send' className='w-44 bg-white border-none text-slate-900 text-xl font-bold p-4 rounded-lg box-border hover:bg-yellow-500'> Submit</button>
 
                 </div>
                 <div className="flex justify-center items-center p-4 mt-8">
-                    <a href="">
+                    <a href="http://linkedin.com/in/kian-zareinajad">
                     <img className="flex w-16 md:w-24 m-2 hover:scale-110" src={linkedin} alt="linkedin" />
                     </a>
-                    <a href="">
+                    <a href="https://github.com/kianZare">
                     <img className="flex w-16 md:w-24 m-2 hover:scale-110" src={github} alt="github" />
 
                     </a>
-                    <a href="">
+                    <a href="mailto:kianzareinejad@gmail.com">
                     <img className="flex w-16 md:w-24 m-2 hover:scale-110" src={email} alt="email" />
 
                     </a>
-                    <a href="">
-                    <img className="flex w-16 md:w-[107px] m-1 hover:scale-110" src={phone} alt="phone" />
+                    <a href="tel:+447508001908">
+                    <img className="flex w-[71px] md:w-[107px] m-1 hover:scale-110" src={phone} alt="phone" />
 
                     </a>
                 </div>
